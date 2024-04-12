600f692bc9d0f566ccd6331e060a30d777dc7f72
chased36 <sebastiancaballero318@aragon.unam.mx>
2024-04-11 21:00:12 -0600

    Mas imagenes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mascota/mascota.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,6 +8,23 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  Future<void> setInitialDate() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('fechaInicio')) {
+      prefs.setString('fechaInicio', DateTime.now().toIso8601String());
+    }
+  }
+
+  Future<int> calcularDias() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey('fechaInicio')) {
+      DateTime fechaInicio = DateTime.parse(prefs.getString('fechaInicio')!);
+      DateTime fechaActual = DateTime.now();
+      return fechaActual.difference(fechaInicio).inDays;
+    }
+    return 0;
+  }
 
   // This widget is the root of your application.
   @override
