fe2bcc78c3d3ffdb1b7cceea98d271e30fb08699
chased36 <sebastiancaballero318@aragon.unam.mx>
2024-04-15 13:48:48 -0600

    Actualizaciones en rama

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mascota/Providers/variableGlobal.dart';
-import 'package:provider/provider.dart';
 import 'package:mascota/mascota.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,40 +8,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  Future<void> setInitialDate() async {
-    final prefs = await SharedPreferences.getInstance();
-    if (!prefs.containsKey('fechaInicio')) {
-      prefs.setString('fechaInicio', DateTime.now().toIso8601String());
-    }
-  }
-
-  Future<int> calcularDias() async {
-    final prefs = await SharedPreferences.getInstance();
-    if (prefs.containsKey('fechaInicio')) {
-      DateTime fechaInicio = DateTime.parse(prefs.getString('fechaInicio')!);
-      DateTime fechaActual = DateTime.now();
-      return fechaActual.difference(fechaInicio).inDays;
-    }
-    return 0;
-  }
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-      return MultiProvider(
-        providers: [
-          ChangeNotifierProvider<VariableGlobal>(create: (_)=>VariableGlobal())
-        ],
-        builder: (context, _) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-              title: 'Mascota',
-              theme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(6, 4, 81, 248)),
-                useMaterial3: true,
-              ),
-              home: const Mascota());
-        }
-      );
+    return MaterialApp(
+        title: 'Mascota',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Mascota());
   }
 }
